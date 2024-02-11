b5881054f69c83591520a3e99d2b44dd83621adf
Roger Leite <roger.barreto@gmail.com>
2024-02-11 19:44:20 -0300

    pass 4nd stage

--- lib/server.ex
+++ lib/server.ex
@@ -16,20 +16,31 @@ defmodule Server do
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     IO.puts("Logs from your program will appear here!")
 
-    {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    {:ok, client} = :gen_tcp.accept(socket)
-    loop(client)
+    {:ok, listen_socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
+    loop_accept(listen_socket)
   end
 
-  def loop(client) do
-    recv_data = :gen_tcp.recv(client, 0)    # Read the incoming data
-    IO.puts("recv data: #{inspect(recv_data)}")
-    case recv_data do
+  def loop_accept(listen_socket) do
+    {:ok, socket} = :gen_tcp.accept(listen_socket)
+
+    spawn(fn ->
+      handle_receive(socket)
+    end)
+
+    loop_accept(listen_socket)
+  end
+
+  def handle_receive(socket) do
+    received_data = :gen_tcp.recv(socket, 0)
+    IO.inspect(received_data, label: :received_data)
+
+    case received_data do
       {:ok, _data} ->
-        :gen_tcp.send(client, "+PONG\r\n")         # Send the PONG response
-        loop(client)                               # Continue the loop
+        :gen_tcp.send(socket, "+PONG\r\n")
+        handle_receive(socket)
+
       {:error, :closed} ->
-        :gen_tcp.close(client)
+        :gen_tcp.close(socket)
     end
   end
 end
