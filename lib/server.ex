d66dc258bfb4f05e3de22b3b3fb96e3da22998cf
Roger Leite <roger.barreto@gmail.com>
2024-02-12 02:28:54 -0300

    pass 6th stage

--- lib/server.ex
+++ lib/server.ex
@@ -2,11 +2,18 @@ defmodule Server do
   @moduledoc """
   Custom implementation of a Redis server
   """
+  alias Server.MemoryStore
 
   use Application
 
   def start(_type, _args) do
-    Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
+    Supervisor.start_link(
+      [
+        {Task, fn -> Server.listen() end},
+        MemoryStore
+      ],
+      strategy: :one_for_one
+    )
   end
 
   @doc """
@@ -41,11 +48,28 @@ defmodule Server do
 
         case String.downcase(cmd) do
           "ping" ->
-            reply = if Enum.empty?(args), do: "+PONG\r\n", else: bulk_string(args)
+            reply = if Enum.empty?(args), do: simple_string("PONG"), else: bulk_string(args)
             :gen_tcp.send(socket, reply |> IO.inspect(label: :reply))
 
           "echo" ->
             reply = bulk_string(args)
+            :gen_tcp.send(socket, reply |> IO.inspect(label: :reply))
+
+          "set" ->
+            [key, value] = args
+            previous_value = MemoryStore.get(key)
+            MemoryStore.set(key, value)
+
+            reply =
+              if is_nil(previous_value),
+                do: simple_string("OK"),
+                else: bulk_string(previous_value)
+
+            :gen_tcp.send(socket, reply |> IO.inspect(label: :reply))
+
+          "get" ->
+            key = args |> List.first()
+            reply = MemoryStore.get(key) |> bulk_string()
             :gen_tcp.send(socket, reply |> IO.inspect(label: :reply))
         end
 
@@ -54,6 +78,14 @@ defmodule Server do
       {:error, :closed} ->
         :gen_tcp.close(socket)
     end
+  end
+
+  defp simple_string(value), do: "+#{value}\r\n"
+
+  defp bulk_string(value) when is_nil(value), do: "$-1\r\n"
+
+  defp bulk_string(value) when is_binary(value) do
+    "$#{String.length(value)}\r\n#{value}\r\n"
   end
 
   # [] => $0\r\n\r\n
@@ -121,4 +153,20 @@ defmodule Server do
 
   defp until_crlf(<<@crlf, rest::binary>>, acc), do: {:ok, acc, rest}
   defp until_crlf(<<byte, rest::binary>>, acc), do: until_crlf(rest, <<acc::binary, byte>>)
+
+  defmodule MemoryStore do
+    use Agent
+
+    def start_link(_opts) do
+      Agent.start_link(fn -> %{} end, name: __MODULE__)
+    end
+
+    def get(key) do
+      Agent.get(__MODULE__, &Map.get(&1, key))
+    end
+
+    def set(key, value) do
+      Agent.update(__MODULE__, &Map.put(&1, key, value))
+    end
+  end
 end
